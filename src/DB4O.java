5c17b8166a8d7da4e6a3939af0eb980ecd5cf875
DichaoWang <dichaowang107@gmail.com>
2023-10-24 21:00:15 +0200

    Query con import com.db4o.query.Query;

--- src/DB4O.java
+++ src/DB4O.java
@@ -1,14 +1,18 @@
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
+import com.db4o.query.Query;
+
 
 public class DB4O {
     public static void main(String[] args) {
         ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "PruebaDB402.yap");
+
         AlumnoCFGS a = new AlumnoCFGS();
         AlumnoCFGS b = new AlumnoCFGS();
         ObjectSet result = db.queryByExample(a);
         ObjectSet result2 = db.queryByExample(b);
+
         while (result.hasNext()) db.delete(result.next());
 
         String[] nombre = {"Juan", "María", "Pedro", "Ana", "Luis", "Marta"};
@@ -21,26 +25,10 @@ public class DB4O {
         for (int i = 0; i < nombre.length; i++)
             db.store(new AlumnoCFGS(nombre[i], edad[i], noGrupo[i], alturaAproxEnM[i], juegaConsola[i], horasEnLOL[i], juegosFavorito[i]));
 
-        /*
-        a.setJuegaEnConsola(true);
-        result = db.queryByExample(a);
-        System.out.println("La query ha devuelto " + result.size() + " alumnos");
-        while (result.hasNext()) db.delete(result.next());
-        result = db.queryByExample(a);
-        System.out.println("Tras la eliminación, quedan " + result.size() + " alumnos que cumpla la query");
+        Query query = db.query();
+        query.descend("juegaConsola").constrain(true).or(query.descend("edad").constrain(21).smaller());
+        ObjectSet<AlumnoCFGS>result3 = query.execute();
+        while (result3.hasNext()) System.out.println(result3.next());
         db.close();
-        */
-        /*
-        a.setNoGrupo((byte)3);
-        b.setNoGrupo((byte)2);
-        result = db.queryByExample(a);
-        result2 = db.queryByExample(b);
-        System.out.println("La query ha devuelto " + (result.size())+(result2.size()) + " alumnos");
-        while (result.hasNext()) System.out.println(result.next());
-        while (result2.hasNext()) System.out.println(result2.next());
-        db.close();
-         */
-
-        
     }
 }
